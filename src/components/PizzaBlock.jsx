45110f25c16ea409ec1e78e8c9558ce191141112
Evgeniy Salish <jeka5650962@gmail.com>
2022-06-21 16:08:25 +0300

    Использовал хук состояния к кнопке добавления пиццы

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,7 +1,12 @@
 import React, {useState} from 'react';
 
-function PizzaBlock({image, title, price}) {
+function PizzaBlock({title, image, price}) {
 
+    const [pizzaCount, setPizzaCount] = useState(0);
+
+    const onClickAdd = () => {
+        setPizzaCount(pizzaCount + 1);
+    };
 
     return (
         <div className="content__column pizza">
@@ -22,14 +27,14 @@ function PizzaBlock({image, title, price}) {
             </div>
             <div className="pizza__bottom">
                 <div className="pizza__price">от {price} ₽</div>
-                <button className="pizza__add">
+                <button onClick={onClickAdd} className="pizza__add">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                             fill="#EB5A1E"/>
                     </svg>
-                    <span>Добавить <b>2</b></span>
+                    <span>Добавить <b>{pizzaCount}</b></span>
                 </button>
             </div>
         </div>
