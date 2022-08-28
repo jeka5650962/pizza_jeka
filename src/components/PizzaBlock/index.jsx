1a632b295024fbacf947bace56335c9d70528f4c
Evgeniy Salish <jeka5650962@gmail.com>
2022-08-28 16:01:38 +0300

    Сверстал пагинацию и сделал onClick UPD_3

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -51,7 +51,9 @@ function PizzaBlock({title, imageUrl, price, sizes, types}) {
                             d="M10.8 4.8H7.2V1.2C7.2 0.5373 6.6627 0 6 0C5.3373 0 4.8 0.5373 4.8 1.2V4.8H1.2C0.5373 4.8 0 5.3373 0 6C0 6.6627 0.5373 7.2 1.2 7.2H4.8V10.8C4.8 11.4627 5.3373 12 6 12C6.6627 12 7.2 11.4627 7.2 10.8V7.2H10.8C11.4627 7.2 12 6.6627 12 6C12 5.3373 11.4627 4.8 10.8 4.8Z"
                             fill="#EB5A1E"/>
                     </svg>
-                    <span>Добавить <b className={pizzaCount > 0 ? 'added' : ''}>{pizzaCount}</b></span>
+                    <span>Добавить {
+                        pizzaCount > 0 && <b style={{display: "inline-flex"}}>{pizzaCount}</b>
+                    }</span>
                 </button>
             </div>
         </div>
